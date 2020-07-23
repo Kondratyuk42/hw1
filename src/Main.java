37e3eaec685312d588a144719bb4d9d1b0ef8868
Kondratyuk <kamiko1409@gmail.com>
2020-07-23 14:12:58 +0300

    homework 2 has been done

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,50 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        float f = 9.8f;
-        int i = 8;
-        byte b = 124;
-        double d = 1.23456789;
-        char c = 'a';
-        boolean bool = true;
-        short s = 2345;
-        long l = 4294967296L;
-       /* System.out.println(f);
-        System.out.println(i);
-        System.out.println(b);
-        System.out.println(d);
-        System.out.println(c);
-        System.out.println(bool);
-        System.out.println(s);
-        System.out.println(l);*/
-        System.out.println(alg(5, 3, 4,2));
-        System.out.println(sum(10, 10));
-        zero(-3);
-        System.out.println(pos_neg(-4));
-        hiName("Камилла");
-        leap(7);
-    }
+        int j;
+       int[] arr = {1, 0, 1, 1, 0, 1, 0, 1, 0, 0};//первое задание
+       for(int i = 0; i<arr.length; i++){
+           if(arr[i]==1){
+               arr[i]=0;
+           }
+           else {
+               arr[i]=1;
+           }
+       }
+        System.out.println(Arrays.toString(arr));
 
-    public static float alg(float a, float b, float c, float d){
-        float alg=a*(b+(c/d));
-        return alg;
-    }
+       int[] arr2 = new int[8];//второе задание
+        for (int i = 0; i <arr2.length ; i++) {
+          arr2[i] = i*3;
+        }
+        System.out.println(Arrays.toString(arr2));
 
-    public static boolean sum(int a, int b){
-        int sum = a + b;
-        return sum>=10 && sum<=20;
-    }
+      int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};// третье задание
+        for (int i = 0; i <arr3.length; i++) {
+         if(arr3[i]<6){
+             arr3[i]*=2;
+         }
+        }
+        System.out.println(Arrays.toString(arr3));
 
-    public static void zero(int a){
-        if (a>=0) System.out.println("Положительное");
-        else System.out.println("Отрицательное");
-    }
+      int[][] arr2d = new int[7][7]; // четвертое задание
+        for(int i=0; i<arr2d.length-1; i++)
+            arr2d[i][i]=1;
+        for(int i=0; i<arr2d.length-1; i++)
+        {
+            j=arr2d.length-1-i;
+            arr2d[i][j]=1;
+        }
+        printarr2d(arr2d);
 
-    public static boolean pos_neg(int a){
-        return a<0;
     }
-
-    public static void hiName(String name) {
-        System.out.println("Привет, " + name);
+    public static void printarr2d(int[][] arr2d) {
+        for (int i = 0; i < arr2d.length; i++) {
+            for (int j = 0; j < arr2d[i].length; j++) {
+                System.out.printf("%5d", arr2d[i][j]);
+            }
+            System.out.println();
+        }
     }
-
-    public static void leap(int a){
-        if ( a%4==0 && a%100!=0 || a%400==0)
-            System.out.println("Високосный");
-        else System.out.println("Не високосный");
-    }
-    }
+}

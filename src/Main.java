e4a4a9948fe53b22ea847e316085879a867a6147
Kondratyuk <kamiko1409@gmail.com>
2020-07-30 15:41:58 +0300

    homework 4 has been done

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,148 @@
-import java.util.Arrays;
+
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    static Random rand = new Random();
+    static final int SIZE = 3;
+//    static final int DOTS_TO_WIN = 3;
+
+    static final char DOT_X = 'X';
+    static final char DOT_O = 'O';
+    static final char DOT_EMPTY = '.';
+
+    static char[][] map;
+
     static Scanner sc = new Scanner(System.in);
+    static Random random = new Random();
 
     public static void main(String[] args) {
-        guess();
+        initMap();
+        printMap();
 
-    }
-    public static void guess(){
-        int k = 0;
-        int x;
-        System.out.println("Угадайте число от 1 до 9");
-        int number = rand.nextInt(10);
-        for(int i = 0; i<3; i++) {
-            int input = sc.nextInt();
-            if(input > number){
-                System.out.println("Загаданное число меньше");
-            }
-            if(input < number){
-                System.out.println("Загаданное число больше");
-            }
-            if(input == number) {
-                System.out.println("ВЫ победили");
-                k++;
+        while (true) {
+            humanTurn();
+            printMap();
+            if(checkWin(DOT_X)){
+                System.out.println("Ты победил! ");
                 break;
             }
-        }
-        if(k==0){
-            System.out.println("Вы проиграли");
-        }
-        System.out.println("Если хотите сыграть еще - нажмите 1, иначе - 0");
-        x = sc.nextInt();
-        if(x==1){
-            guess();
+            if (isFull()) {
+                System.out.println("Ничья!");
+                break;
+            }
+
+            aiTurn();
+            printMap();
+            if(checkWin(DOT_O)){
+                System.out.println("Компьютер победил! ");
+                break;
+            }
+            if (isFull()) {
+                System.out.println("Ничья!");
+                break;
+            }
+
         }
     }
+
+
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
+    public static void printMap() {
+        System.out.print("  ");
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print(i + 1 + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print(i + 1 + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void humanTurn() {
+        int x, y;
+
+        do {
+            System.out.println("Input X, Y ");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(y, x));
+
+        map[y][x] = DOT_X;
+    }
+
+    public static boolean isCellValid(int y, int x) {
+        if (x < 0 || y < 0 || x >= SIZE || y >= SIZE) {
+            return false;
+        }
+        return map[y][x] == DOT_EMPTY;
+    }
+
+    public static void aiTurn() {
+        int x, y;
+
+        do {
+            x = random.nextInt(SIZE);
+            y = random.nextInt(SIZE);
+        } while (!isCellValid(y, x));
+
+        map[y][x] = DOT_O;
+    }
+
+    public static boolean isFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkWin(char c) {
+        for(int i=0; i<3; i++){
+            int k=0;
+            for (int j = 0; j < 3 ; j++) {
+                if(map[i][j]==c) {k++; }
+            }
+            if(k==3) {return true; }
+        }
+
+        for(int j=0; j<3; j++){
+            int k=0;
+            for (int i = 0; i < 3 ; i++) {
+                if(map[i][j]==c) {k++; }
+            }
+            if(k==3) {return true; }
+        }
+        int h=0;
+        for (int i = 0; i < 3; i++) {
+            if(map[i][i]==c) {h++; }
+            if(h==3) {return true; }
+        }
+        h = 0;
+        for(int i=0; i<3; i++){
+            for (int j = 2; j >= 0 ; j--) {
+                if(map[i][j]==c) {h++; }
+            }
+            if(h==3) {return true; }
+        }
+
+
+        return false;
+    }
+
 }
